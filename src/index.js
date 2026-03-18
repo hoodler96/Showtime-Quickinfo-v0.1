57a6a99a5e11958f7744d69ab50466693d107620
hoodler96 <mwinvestmentsolutionsinc@gmail.com>
2026-03-17 21:50:57 -0400

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const mongoose = require('mongoose');
+const fs = require('fs');
+const path = require('path');
+const { startAlertPoller } = require('./jobs/alertPoller');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.DirectMessages,
+  ],
+});
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(path.join(commandsPath, file));
+  if ('data' in command && 'execute' in command) {
+    client.commands.set(command.data.name, command);
+  }
+}
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+  const event = require(path.join(eventsPath, file));
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
+}
+
+async function bootstrap() {
+  await mongoose.connect(process.env.MONGODB_URI);
+  console.log('Mongo connected');
+
+  await client.login(process.env.DISCORD_TOKEN);
+  startAlertPoller(client);
+}
+
+bootstrap().catch(err => {
+  console.error('Bootstrap failed:', err);
+  process.exit(1);
+});
