e767425d74bb8821b107d1051470e20959ce19b8
hoodler96 <mwinvestmentsolutionsinc@gmail.com>
2026-03-17 22:02:30 -0400

    Update ask.js

--- src/commands/ask.js
+++ src/commands/ask.js
@@ -1,1 +1,21 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { askQuickinfo } = require('../lib/openai');
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('ask')
+    .setDescription('Ask Showtime Quickinfo a market question')
+    .addStringOption(option =>
+      option.setName('prompt')
+        .setDescription('Your market question')
+        .setRequired(true)
+    ),
+
+  async execute(interaction) {
+    const prompt = interaction.options.getString('prompt');
+    await interaction.deferReply();
+
+    const answer = await askQuickinfo(prompt);
+    await interaction.editReply(answer);
+  },
+};
