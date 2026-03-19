c0de3b69e39cdd82b2a7e2a2f9740c281b5b6633
hoodler96 <mwinvestmentsolutionsinc@gmail.com>
2026-03-18 23:07:36 -0400

    Update greeks.js

--- src/commands/greeks.js
+++ src/commands/greeks.js
@@ -1,1 +1,6 @@
-
+`IV: ${contract.impliedVolatility}`
+`Last Price: ${contract.lastPrice}`
+`Bid: ${contract.bid}`
+`Ask: ${contract.ask}`
+`Volume: ${contract.volume}`
+`Open Interest: ${contract.openInterest}`
