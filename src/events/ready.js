259a510c2da5e9b12781d5d86a678d9c62f4c44a
hoodler96 <mwinvestmentsolutionsinc@gmail.com>
2026-07-10 01:59:44 -0400

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,9 @@
+const startAlertPoller = require('../jobs/alertPoller');
 module.exports = {
   name: 'ready',
   once: true,
   execute(client) {
+    startAlertPoller(client);
     console.log(`Logged in as ${client.user.tag}`);
   },
 };
